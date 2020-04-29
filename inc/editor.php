6b6774a394dc0e56aa311b50b0e006cac01d013e
Josh Armentano <josh@abidewebdesign.com>
2020-04-29 10:35:25 -0700

    Fixed bug.

--- inc/editor.php
+++ inc/editor.php
@@ -251,10 +251,12 @@ function change_stripe_description( $description, $strings, $entry ) {
 }
 
 // Changes Gravity Forms Ajax Spinner (next, back, submit) to a transparent image
-// this allows you to target the css and create a pure css spinner like the one used below in the style.css file of this gist.
 add_filter( 'gform_ajax_spinner_url', 'spinner_url', 10, 2 );
 function spinner_url( $image_src, $form ) {
     
     return  'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'; // relative to you theme images folder
 
 }
+
+// Remove scroll to top functionality on multi-page forms.
+add_filter( 'gform_confirmation_anchor', '__return_false' );
