c8671ee37953d96bda438beb35e29fd31b362bb9
Josh Armentano <josh@abidewebdesign.com>
2020-05-21 08:44:14 -0700

    Remove post type.

--- inc/setup.php
+++ inc/setup.php
@@ -97,6 +97,37 @@ function remove_dashboard_widgets() {
 add_action('wp_dashboard_setup', 'remove_dashboard_widgets' );
 
 /*
+ * Remove unused post type
+ *
+ */
+// Remove side menu
+add_action( 'admin_menu', 'remove_default_post_type' );
+
+function remove_default_post_type() {
+	
+	remove_menu_page( 'edit.php' );
+
+}
+
+// Remove +New post in top Admin Menu Bar
+add_action( 'admin_bar_menu', 'remove_default_post_type_menu_bar', 999 );
+
+function remove_default_post_type_menu_bar( $wp_admin_bar ) {
+	
+	$wp_admin_bar->remove_node( 'new-post' );
+
+}
+
+// Remove Quick Draft Dashboard Widget
+add_action( 'wp_dashboard_setup', 'remove_draft_widget', 999 );
+
+function remove_draft_widget(){
+
+	remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+
+}
+
+/*
  * Add custom favicon to admin pages
  *
  */
